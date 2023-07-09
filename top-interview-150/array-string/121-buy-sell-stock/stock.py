0a2430f046ae290740b93905b95fefe4a520b2ea
Karan Kumar <karan@karankumar.com>
2023-07-09 14:18:22 +0100

    Remove redundant validation check

--- top-interview-150/array-string/121-buy-sell-stock/stock.py
+++ top-interview-150/array-string/121-buy-sell-stock/stock.py
@@ -3,10 +3,6 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        if len(prices) < 2:
-            # need at least 2 days: one to buy and one to sell
-            return 0
-
         best_buy_index = 0
         max_profit = 0
         for i in range(1, len(prices)):
