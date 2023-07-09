b97602890aad8f57fea5abe7c04eaf2e23c517d9
Karan Kumar <karan@karankumar.com>
2023-07-09 14:10:03 +0100

    Simplify best sell stock solution

--- top-interview-150/array-string/121-buy-sell-stock/stock.py
+++ top-interview-150/array-string/121-buy-sell-stock/stock.py
@@ -8,7 +8,6 @@ class Solution:
             return 0
 
         best_buy_index = 0
-        best_sell_index = 1  # can only sell after buying
         max_profit = 0
         shift = 1
         for index, val in enumerate(prices[shift:]):
@@ -18,9 +17,6 @@ class Solution:
 
             current_profit = prices[shifted_index] - prices[best_buy_index]
             if shifted_index > best_buy_index and current_profit > max_profit:
-                best_sell_index = shifted_index
                 max_profit = current_profit
 
-        print(f"Best buy: {prices[best_buy_index]}")
-        print(f"Best sell: {prices[best_sell_index]}")
         return max_profit
