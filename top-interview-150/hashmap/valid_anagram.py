56f8866f91a3e1478b3b309698b9df2fe495e674
Karan Kumar <karan@karankumar.com>
2023-07-09 09:40:50 +0100

    Compare counters for one line solution

--- top-interview-150/hashmap/valid_anagram.py
+++ top-interview-150/hashmap/valid_anagram.py
@@ -4,20 +4,4 @@ from collections import Counter
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-
-        s_counter = Counter(s)
-        t_counter = Counter(t)
-
-        # # check if every letter in s is in t and occurs the same number of times
-        for k, v in s_counter.items():
-            if k not in t_counter:
-                # letter in s but not in t
-                return False
-
-            if v != t_counter[k]:
-                # a letter in s occurs a different number of times to the same letter in t
-                return False
-
-        return True
+        return Counter(s) == Counter(t)
