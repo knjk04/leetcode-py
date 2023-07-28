5f6b60b0c0a2d48835354cdbcd2838148a82680d
Karan Kumar <knjk04@users.noreply.github.com>
2023-07-28 14:03:11 +0100

    Use no additional list

--- running_sum_1d_array_1480.py
+++ running_sum_1d_array_1480.py
@@ -4,9 +4,7 @@ from typing import List
 # Problem: https://leetcode.com/problems/running-sum-of-1d-array/description/
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
-        running_sum = [nums[0]]
-
         for i in range(1, len(nums)):
-            running_sum.append(nums[i] + running_sum[i - 1])
+            nums[i] = nums[i] + nums[i - 1]
 
-        return running_sum
+        return nums
