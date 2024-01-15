3be6c1db578cfcd46c17306409e218ed490dba2a
Karan Kumar <knjk04@users.noreply.github.com>
2024-01-15 08:32:15 +0000

    Add valid_anagram solution

---
+++ grind-75/week1/valid_anagram.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+from typing import Dict
+
+
+class Solution:
+    def count_occurrences(self, s: str) -> Dict[str, int]:
+        occurrences = defaultdict(int)
+        for letter in s:
+            occurrences[letter] += 1
+        return occurrences
+
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        return self.count_occurrences(s) == self.count_occurrences(t)
